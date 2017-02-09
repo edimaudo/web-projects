76fdc4b55929b7637cabaf388cb42d5eba36ec34
Edima Udo <edimaudo@gmail.com>
2017-02-09 15:31:53 -0500

    Update

--- algorithms/freecodecamp/Basic_Algorithm_Scripting/confirm the ending.js
+++ algorithms/freecodecamp/Basic_Algorithm_Scripting/confirm the ending.js
@@ -1,12 +1,14 @@
 /*
 Check if a string (first argument) ends with the given target string (second argument).
 */
-function end(str, target) {
+function confirmEnding(str, target) {
    var output = str.split(" ");
   //return output;
   if (output.length < 2){
     return target === output[0].slice(output.lastIndexOf(target),str.length)? true:false;
   } else {
-    return target === output[output.length-1]? true:false;
+    lastValue = output[output.length - 1];
+    return target === lastValue.substring(lastValue.length - target.length,lastValue.length)? true:false;
+    //return target === output[output.length-1]? true:false;
   }
 }
