99a6b32cd9c99a72133f04f2cdbe355cf3e1c83a
Edima Udo <edimaudo@gmail.com>
2017-02-11 19:25:13 -0500

    update

--- algorithms/freecodecamp/Intermediate_ Algorithm_Scripting/Everything be true.js
+++ algorithms/freecodecamp/Intermediate_ Algorithm_Scripting/Everything be true.js
@@ -2,12 +2,25 @@
 Check if the predicate (second argument) returns truthy (defined) for all elements of a collection (first argument).
 */
 
-function every(collection, pre) {
-  var count = 0;
-  for (var i = 0; i < collection.length; i++) {
-    if (collection[0].hasOwnProperty(pre)) {
-      count += 1;
-    }
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+	var count = 0;
+  
+  for (var i = 0; i < collection.length; i++){
+			if(collection[i].hasOwnProperty(pre)){
+      	if (typeof collection[i][pre] === "undefined" || collection[i][pre] === null || collection[i][pre]==="" ||
+        collection[i][pre] === 0){
+        	count+=0;
+        } else if (valueIsNaN(collection[i][pre])){
+        	count+=0;
+        } else {
+        		count+=1;
+        }
+      }
   }
-  return count !== collection.length ? false : true;
+  
+  //alert(count + " " + collection.length);
+  return count !== collection.length? false: true;
 }
+
+function valueIsNaN(v) { return v !== v; }
