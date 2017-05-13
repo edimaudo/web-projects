e15eadb79fc9aec34569eb741062a603ec6a95bb
Edima Udo <edimaudo@gmail.com>
2017-05-13 12:55:50 -0400

    twitterClone
    removed libraries and updated main JS, HTML and CSS

--- web_apps/twitterClone/client/main.js
+++ web_apps/twitterClone/client/main.js
@@ -1,22 +1,87 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+Tweets = new Meteor.Collection("tweets");
 
-import './main.html';
+if (Meteor.isClient) {
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+  Template.userManagement.helpers({
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+  Template.userManagement.events({
+    'click #signup': function() {
+      var user = {
+        username: $('#signup-username').val(),
+        password: $('#signup-password').val(),
+        profile: {
+          fullname: $('#signup-fullname').val()
+        }
+      };
+
+      Accounts.createUser(user, function (error) {
+        if(error) alert(error);
+      });
+    },
+
+    'click #login': function() {
+      var username = $('#login-username').val();
+      var password = $('#login-password').val();
+
+      Meteor.loginWithPassword(username, password, function(error) {
+        if(error) alert(error);
+      });
+    },
+
+    'click #logout': function() {
+      Meteor.logout();
+    }
+  });
+
+
+
+
+  Template.tweetBox.helpers({
+    charCount: function() {
+      return 140 - Session.get('numChars');
+    },
+
+    charClass: function() {
+      if (Session.get('numChars') > 140) {
+        return 'errCharCount';
+      } else {
+        return 'charCount';
+      }
+    },
+
+    disableButton: function() {
+      if (Session.get('numChars') <= 0 ||
+          Session.get('numChars') > 140 ||
+          !Meteor.user()) {
+        return 'disabled';
+      }
+    }
+  });
+
+  Template.tweetBox.events({
+    'input #tweetText': function(){
+      Session.set('numChars', $('#tweetText').val().length);
+    },
+
+    'click button': function() {
+      var tweet = $('#tweetText').val();
+      $('#tweetText').val("");
+      Session.set('numChars', 0);
+      if (Meteor.user()) {
+        Tweets.insert({message: tweet, user: Meteor.user().username});
+      }
+    }
+  });
+
+  Template.tweetBox.onRendered(function () {
+    Session.set('numChars', 0);
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
