1a5043f34e1155e6be33f0c98539ea2471af0883
Edima Udo <edimaudo@gmail.com>
2018-04-09 16:14:35 -0400

    event_manager
    added sunlight gem and api key as well as added information to get the
    legislators

--- jumpstartlabs/event_manager/lib/event_manager.rb
+++ jumpstartlabs/event_manager/lib/event_manager.rb
@@ -1,5 +1,9 @@
 #event engagement app - A number of people have registered for an upcoming event. She has asked for your help in engaging these future attendees.
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -12,9 +16,9 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
 
-  zipcode = clean_zipcode(row[:zipcode])
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
 
 
