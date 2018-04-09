158bafb496b458e72d2d6024160e737985dea40f
Edima Udo <edimaudo@gmail.com>
2018-04-09 16:06:14 -0400

    event_manager
    handling empty zip code

--- jumpstartlabs/event_manager/lib/event_manager.rb
+++ jumpstartlabs/event_manager/lib/event_manager.rb
@@ -1,5 +1,6 @@
 #event engagement app - A number of people have registered for an upcoming event. She has asked for your help in engaging these future attendees.
-require "csv"
+require 'csv'
+
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
@@ -8,7 +9,9 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
 
-  if zipcode.length < 5
+  if zipcode.nil?
+    zipcode = "00000"
+  elsif zipcode.length < 5
     zipcode = zipcode.rjust 5, "0"
   elsif zipcode.length > 5
     zipcode = zipcode[0..4]
