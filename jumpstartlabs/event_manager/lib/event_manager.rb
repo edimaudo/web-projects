89730b402f5ae9347d78c25175948f6f638b37bd
Edima Udo <edimaudo@gmail.com>
2018-04-09 16:17:06 -0400

    event_manager
    moved the legislator code into a function

--- jumpstartlabs/event_manager/lib/event_manager.rb
+++ jumpstartlabs/event_manager/lib/event_manager.rb
@@ -4,9 +4,18 @@ require 'sunlight/congress'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislator_names.join(", ")
 end
 
 puts "EventManager initialized."
@@ -16,13 +25,11 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
 
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
+  zipcode = clean_zipcode(row[:zipcode])
 
-  legislators_string = legislator_names.join(", ")
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators_string}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
 
 
