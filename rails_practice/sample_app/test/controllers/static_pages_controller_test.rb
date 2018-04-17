f0aef5aebdf00eabc9ea48de4667a848dad72541
Edima Udo <edimaudo@gmail.com>
2018-04-17 13:58:49 -0400

    sample app
    added test for root

--- rails_practice/sample_app/test/controllers/static_pages_controller_test.rb
+++ rails_practice/sample_app/test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionController::TestCase
   	@base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get :home
+    assert_response :success
+  end
+
   test "should get home" do
     get :home
     assert_response :success
@@ -28,5 +33,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+
+
 
 end
