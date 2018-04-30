d07a6d693567c3ae70b547be69edab31d5dfb287
Edima Udo <edimaudo@gmail.com>
2018-04-30 17:10:13 -0400

    jumper news
    updated application controller

--- rails_project/jump_news/app/controllers/application_controller.rb
+++ rails_project/jump_news/app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def login(user)
+  session[:user_id] = user.id
+end
+
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id])
+end
+
+def logout
+  session.delete(:user_id)
+  @current_user = nil
+end
+
+def logged_in?
+  current_user.nil? ? false : true
+end
+
+helper_method :current_user, :logged_in?
 end
