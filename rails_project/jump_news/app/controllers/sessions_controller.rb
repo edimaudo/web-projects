4b4a88134c222d486f48a7e46f74387ad927e348
Edima Udo <edimaudo@gmail.com>
2018-04-30 17:17:31 -0400

    jumper news
    added destroy

--- rails_project/jump_news/app/controllers/sessions_controller.rb
+++ rails_project/jump_news/app/controllers/sessions_controller.rb
@@ -3,20 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-  user = User.find_by(username: login_params[:username])
+    user = User.find_by(username: login_params[:username])
 
-  if user && user.authenticate(login_params[:password])
-    login(user)
-    redirect_back fallback_location: new_session_path, notice: 'Logged in'
-  else
-    flash.now[:notice] = 'Invalid username / password combination'
-    render :new
+    if user && user.authenticate(login_params[:password])
+      login(user)
+      redirect_back fallback_location: new_session_path, notice: 'Logged in'
+    else
+      flash.now[:notice] = 'Invalid username / password combination'
+      render :new
+    end
   end
-end
+
+  def destroy
+    logout
+    redirect_back fallback_location: new_session_path, notice: 'Logged out'
+  end
 
 private
 
-def login_params
-  params.require(:session).permit(:username, :password)
-end
+  def login_params
+    params.require(:session).permit(:username, :password)
+  end
 end
