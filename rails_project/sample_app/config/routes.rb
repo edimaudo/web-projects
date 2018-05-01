b469e7a785a9a9651cf5c6ba3362928eeaa100a6
Edima Udo <edimaudo@gmail.com>
2018-05-01 14:29:42 -0400

    sample app
    created user using rails generate controller Users new

--- rails_project/sample_app/config/routes.rb
+++ rails_project/sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   #get 'static_pages/home'
 
   #get 'static_pages/help'
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
   #get 'static_pages/contact'
 
   root "static_pages#home"
+
 
   get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
--- rails_project/sample_app/test/integration/site_layout_test.rb
+++ rails_project/sample_app/test/integration/site_layout_test.rb
@@ -7,9 +7,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    #assert_select "a[href=?]", 'root', count: 2
+    assert_select "a[href=?]", 'help'
+    assert_select "a[href=?]", 'about'
+    assert_select "a[href=?]", 'contact'
+    
   end
 end
