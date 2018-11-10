0397cae9f3b5c005f71de6e298f3730000a07a79
Edima Udo <edimaudo@gmail.com>
2018-11-10 17:21:00 -0500

    updated login forms

--- flask_project/app/routes.py
+++ flask_project/app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -33,10 +35,14 @@ def index():
  #    </body>
 	# </html>'''
 
- @app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)   
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form) 
 
 @app.route('/main')
 def main():
