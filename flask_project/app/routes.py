aa378cf1d3981d5c18fb76a49fd0b2ae36ede829
Edima Udo <edimaudo@gmail.com>
2018-11-10 17:14:17 -0500

    added login form

--- flask_project/app/routes.py
+++ flask_project/app/routes.py
@@ -1,11 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+    #user = {'username': 'Miguel'}
+    #return render_template('index.html', title='Home', user=user)
 
  # this style of code is bad because you would have to change your template everything your code changes
  # a better option is to use a template
@@ -19,7 +32,11 @@ def index():
  #        <h1>Hello, ''' + user['username'] + '''!</h1>
  #    </body>
 	# </html>'''
-    
+
+ @app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)   
 
 @app.route('/main')
 def main():
