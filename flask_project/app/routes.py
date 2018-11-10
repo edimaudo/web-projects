fa30783a1ce38a2be6def2931964f99fb3f83488
Edima Udo <edimaudo@gmail.com>
2018-11-10 16:45:40 -0500

    updated flask app

--- flask_project/app/routes.py
+++ flask_project/app/routes.py
@@ -4,7 +4,22 @@ from app import app
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
+
+ # this style of code is bad because you would have to change your template everything your code changes
+ # a better option is to use a template
+ #    user = {'username': 'Miguel'}
+ #    return '''
+ #    <html>
+ #    <head>
+ #        <title>Home Page - Microblog</title>
+ #    </head>
+ #    <body>
+ #        <h1>Hello, ''' + user['username'] + '''!</h1>
+ #    </body>
+	# </html>'''
+    
 
 @app.route('/main')
 def main():
