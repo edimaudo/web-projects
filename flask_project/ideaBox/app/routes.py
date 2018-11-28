0dfff2400a3ccc1a6bc076e39c716436bd81630b
Edima Udo <edimaudo@gmail.com>
2018-11-27 19:49:32 -0500

    update

--- flask_project/ideaBox/app/routes.py
+++ flask_project/ideaBox/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
