5212e05debf21f7427973c3b0e6438cd10f27f53
Edima Udo <edimaudo@gmail.com>
2018-12-02 20:51:10 -0500

    update

--- flask_project/ideaBox/ideabox.py
+++ flask_project/ideaBox/ideabox.py
@@ -7,19 +7,63 @@ from flask_sqlalchemy import SQLAlchemy #orm
 import os
 
 #added db
-#project_dir = os.path.dirname(os.path.abspath(__file__))
-#database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "ideaBox.db"))
 
 
 app = Flask(__name__)
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+class Idea(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    description = db.Column(db.String(140), unique=False, nullable=False)
+
+    def __repr__(self):
+    	return "<Idea {}>".format(self.title)
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	return render_template("index.html")
+    ideas = None
+    if request.form:
+        try:
+            idea = Idea(title=request.form.get("title"))
+            description = Idea(description = request.form.get("description"))
+            db.session.add(idea)
+            db.session.add(description)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add idea")
+            print(e)
+    ideas = Idea.query.all()
+    return render_template("index.html", ideas=ideas)
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        newdescription = request.form.get("newdescription")
+        idea = Idea.query.filter_by(title=oldtitle).first()
+        idea.title = newtitle
+        idea.description = newdescription
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update idea")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    idea = Idea.query.filter_by(title=title).first()
+    db.session.delete(idea)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
