cb2c491cbc59b4161b98742dc03d412ab04ea137
Edima Udo <edimaudo@gmail.com>
2018-12-11 18:50:52 -0500

    ideaBox
    simple crud app to keep track of ideas using flask

--- flask_project/ideaBox/ideabox.py
+++ flask_project/ideaBox/ideabox.py
@@ -18,26 +18,24 @@ app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
 
 class Idea(db.Model):
-    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
-    description = db.Column(db.String(140), unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=False, nullable = False)
+    description = db.Column(db.String(140), unique=False, nullable = False)
 
     def __repr__(self):
-    	return "<Idea {}>".format(self.title)
+    	return "<Idea {}>".format(self.title, self.description)
 
-
-
+#ADD
 @app.route("/", methods=["GET", "POST"])
 def index():
     ideas = None
     if request.form:
         try:
-            idea = Idea(title=request.form.get("title"))
-            description = Idea(description = request.form.get("description"))
+            idea = Idea(title=request.form.get("title"), description=request.form.get("description"))
             db.session.add(idea)
-            db.session.add(description)
             db.session.commit()
         except Exception as e:
-            print("Failed to add idea")
+            print("Failed to add Idea")
             print(e)
     ideas = Idea.query.all()
     return render_template("index.html", ideas=ideas)
@@ -45,24 +43,29 @@ def index():
 @app.route("/update", methods=["POST"])
 def update():
     try:
-        newtitle = request.form.get("newtitle")
         oldtitle = request.form.get("oldtitle")
+        olddescription = request.form.get("olddescription")
+        newtitle = request.form.get("newtitle")
         newdescription = request.form.get("newdescription")
         idea = Idea.query.filter_by(title=oldtitle).first()
         idea.title = newtitle
         idea.description = newdescription
         db.session.commit()
     except Exception as e:
-        print("Couldn't update idea")
+        print("Failed to update idea")
         print(e)
     return redirect("/")
 
 @app.route("/delete", methods=["POST"])
 def delete():
-    title = request.form.get("title")
-    idea = Idea.query.filter_by(title=title).first()
-    db.session.delete(idea)
-    db.session.commit()
+    try:
+        iid = request.form.get("idea_id")
+        idea = Idea.query.filter_by(id = iid).first()
+        db.session.delete(idea)
+        db.session.commit()
+    except Exception as e:
+        print("Failed to delete idea")
+        print(e)
     return redirect("/")
 
 if __name__ == "__main__":
