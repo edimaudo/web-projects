008a7acac629682a888ba0be3ab329d86308a2b5
Edima Udo <edimaudo@gmail.com>
2019-03-20 06:59:35 -0400

    update

--- flask_project/musicdb/main.py
+++ flask_project/musicdb/main.py
@@ -33,6 +33,79 @@ def search_results(search):
     else:
         # display results
         return render_template('results.html', results=results)
+
+@app.route('/new_album', methods=['GET', 'POST'])
+def new_album():
+    """
+    Add a new album
+    """
+    form = AlbumForm(request.form)
  
+    if request.method == 'POST' and form.validate():
+        # save the album
+        album = Album()
+        save_changes(album, form, new=True)
+        flash('Album created successfully!')
+        return redirect('/')
+ 
+    return render_template('new_album.html', form=form)
+
+def save_changes(album, form, new=False):
+    """
+    Save the changes to the database
+    """
+    # Get data from form and assign it to the correct attributes
+    # of the SQLAlchemy table object
+    artist = Artist()
+    artist.name = form.artist.data
+ 
+    album.artist = artist
+    album.title = form.title.data
+    album.release_date = form.release_date.data
+    album.publisher = form.publisher.data
+    album.media_type = form.media_type.data
+ 
+    if new:
+        # Add the new album to the database
+        db_session.add(album)
+ 
+    # commit the data to the database
+    db_session.commit()
+
+@app.route('/results')
+def search_results(search):
+    results = []
+    search_string = search.data['search']
+ 
+    if search.data['search'] == '':
+        qry = db_session.query(Album)
+        results = qry.all()
+ 
+    if not results:
+        flash('No results found!')
+        return redirect('/')
+    else:
+        # display results
+        table = Results(results)
+        table.border = True
+        return render_template('results.html', table=table)
+
+@app.route('/item/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    qry = db_session.query(Album).filter(
+                Album.id==id)
+    album = qry.first()
+ 
+    if album:
+        form = AlbumForm(formdata=request.form, obj=album)
+        if request.method == 'POST' and form.validate():
+            # save edits
+            save_changes(album, form)
+            flash('Album updated successfully!')
+            return redirect('/')
+        return render_template('edit_album.html', form=form)
+    else:
+        return 'Error loading #{id}'.format(id=id)
+
 if __name__ == '__main__':
     app.run()
