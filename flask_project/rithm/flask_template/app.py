a64bf0cbca70814115da8c17cbb18036487f2145
Edima Udo <edimaudo@gmail.com>
2019-03-23 15:34:51 -0400

    Update app.py

---
+++ flask_project/rithm/flask_template/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/person/<person>/<int:age>')
+def say_name_age(person,age):
+	return render_template("person.html",person=person,age=age)
