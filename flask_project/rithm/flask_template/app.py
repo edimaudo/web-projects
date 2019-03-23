2cf886a468219aaef4f50a085b9f12a21e1ebed1
Edima Udo <edimaudo@gmail.com>
2019-03-23 15:43:07 -0400

    Update app.py

--- flask_project/rithm/flask_template/app.py
+++ flask_project/rithm/flask_template/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/person/<person>/<int:age>')
 def say_name_age(person,age):
 	return render_template("person.html",person=person,age=age)
+
+@app.route('/calculate')
+def calculate():
+	return render_template("calc.html")
