ac788ca7c92e60db5cb4c8c236b79b8006ddfde5
Edima Udo <edimaudo@gmail.com>
2019-03-23 15:06:16 -0400

    Update app.py

--- flask_project/rithm/app.py
+++ flask_project/rithm/app.py
@@ -7,6 +7,10 @@ def welcome():
     random_name = "Tom"
     return render_template('index.html', names=names_of_instructors, name=random_name)
 
+@app.route('/title')
+def title():
+    return render_template('title.html')
+
 @app.route('/second')
 def second():
     return "WELCOME TO THE SECOND PAGE!"
