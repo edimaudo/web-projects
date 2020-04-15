04de88cb9f34c68ea02d8c03f99e8cb6e7c3bc07
Edima <edimaudo@gmail.com>
2020-04-15 16:19:59 -0400

    Update add_learning.php

--- fullstack_side_projects/learning_journal/add_learning.php
+++ fullstack_side_projects/learning_journal/add_learning.php
@@ -3,7 +3,14 @@
 $page_title = "Add learning";
 include_once "header.php";
  
-// contents will be here
+// add learning button
+echo "<div class='right-button-margin'>";
+    echo "<a href='index.php' class='btn btn-default pull-right'>Read Products</a>";
+echo "</div>";
+ 
+?>
+<!-- 'create product' html form will be here -->
+<?php
  
 // footer
 include_once "footer.php";
