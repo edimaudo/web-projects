f07601197b408cf9a726f1406b278a3d3a86937b
Edima <edimaudo@gmail.com>
2020-04-18 14:48:25 -0400

    Update learn.php

--- fullstack_side_projects/learning_journal/objects/learn.php
+++ fullstack_side_projects/learning_journal/objects/learn.php
@@ -50,23 +50,6 @@
          
             }
 
-/*            function readAll(){
-             
-                $query = "SELECT
-                            id, title, time_spent, learning, resources
-                        FROM
-                            " . $this->table_name . "
-                        ORDER BY
-                            title ASC";
-                        //LIMIT
-                        //    {$from_record_num}, {$records_per_page}
-             
-                $stmt = $this->conn->prepare( $query );
-                $stmt->execute();
-             
-                return $stmt;
-            }*/
-
             function readAll($from_record_num, $records_per_page){
              
                 $query = "SELECT
@@ -95,6 +78,66 @@
                 $num = $stmt->rowCount();
              
                 return $num;
+            }
+
+            function readOne(){
+ 
+                $query = "SELECT
+                            title, time_spent, learning, resources
+                        FROM
+                            " . $this->table_name . "
+                        WHERE
+                            id = ?
+                        LIMIT
+                            0,1";
+             
+                $stmt = $this->conn->prepare( $query );
+                $stmt->bindParam(1, $this->id);
+                $stmt->execute();
+             
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+             
+                $this->title = $row['title'];
+                $this->time_spent = $row['time_spent'];
+                $this->learning = $row['learning'];
+                $this->resources = $row['resources'];
+            }
+
+            function update(){
+             
+                $query = "UPDATE
+                            " . $this->table_name . "
+                        SET
+                            title = :title,
+                            time_spent = :time_spent,
+                            learning = :learning,
+                            resources  = :resources
+                        WHERE
+                            id = :id";
+             
+                $stmt = $this->conn->prepare($query);
+             
+                // posted values
+                $this->title=htmlspecialchars(strip_tags($this->title));
+                $this->time_spent=htmlspecialchars(strip_tags($this->time_spent));
+                $this->learning=htmlspecialchars(strip_tags($this->learning));
+                $this->resources=htmlspecialchars(strip_tags($this->resources));
+                $this->id=htmlspecialchars(strip_tags($this->id));
+             
+                // bind parameters
+                $stmt->bindParam(':title', $this->title);
+                $stmt->bindParam(':time_spent', $this->time_spent);
+                $stmt->bindParam(':learning', $this->learning);
+                $stmt->bindParam(':resources', $this->resources);
+                $stmt->bindParam(':id', $this->id);
+             
+                // execute the query
+                if($stmt->execute()){
+                    return true;
+                }
+             
+                return false;
+                 
             }
 
     }
