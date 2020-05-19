75f1554820535da4ef855798dc41082411793471
Edima <edimaudo@gmail.com>
2020-05-19 17:47:01 -0400

    updates

--- fullstack_side_projects/LevelUp/locallibrary/catalog/models.py
+++ fullstack_side_projects/LevelUp/locallibrary/catalog/models.py
@@ -4,6 +4,10 @@ import uuid
 from datetime import date
 
 # Create your models here.
+
+#These are based on the different tables in the database 
+#it will contain the information
+
 class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
