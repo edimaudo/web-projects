3905900fac768a01a8b423deb73692b5adeaf5f9
Edima <edimaudo@gmail.com>
2020-05-19 11:55:00 -0400

    practice

---
+++ django_apps/djangocrud/coins/models.py
@@ -0,0 +1,12 @@
+
+
+# Create your models here.
+from django.db import models
+
+class Coin(models.Model):
+    coin_name = models.CharField(max_length=30)
+    coin_price = models.IntegerField()
+    created_at = models.DateField()
+
+    def __str__(self):
+    	return self.coin_name
