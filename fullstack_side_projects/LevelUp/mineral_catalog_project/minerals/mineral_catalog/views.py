e7e879e6e5dded5b416db8dee0a4b69c38455f1c
Edima <edimaudo@gmail.com>
2020-05-27 16:46:29 -0400

    Update views.py

--- fullstack_side_projects/LevelUp/mineral_catalog_project/minerals/mineral_catalog/views.py
+++ fullstack_side_projects/LevelUp/mineral_catalog_project/minerals/mineral_catalog/views.py
@@ -7,16 +7,15 @@ from .models import Mineral
 def index(request):
 	return render(request, 'mineral_catalog_list.html')
 
-def mineral_catalog(request):
-	"""View function for home page of site."""
-	return render(request, 'mineral_catalog_list.html')
+#def mineral_catalog(request):
+#	"""View function for home page of site."""
+#	return render(request, 'mineral_catalog_list.html')
 
 
-class MineralListView(generic.ListView):
-    model = Mineral
-    context_object_name = 'my_mineral_list'   # your own name for the list as a template variable
-    queryset = Mineral.objects.all() # Get 5 books containing the title war
-    template_name = 'mineral_catalog_list.html'  # Specify your own template name/location
+def mineral_catalog_list(request):
+	m_list = Mineral.objects.all()
+	context = {'mineral_list':m_list}
+	return render(request, 'mineral_catalog_list.html',context)
 
 
 
