a290d1b951da191434d0529cbab478af3cf8ffb0
Edima <edimaudo@gmail.com>
2020-05-27 16:46:26 -0400

    Update urls.py

--- fullstack_side_projects/LevelUp/mineral_catalog_project/minerals/mineral_catalog/urls.py
+++ fullstack_side_projects/LevelUp/mineral_catalog_project/minerals/mineral_catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mineral_catalog, name='index'),
-    path('mineral_catalog',views.mineral_catalog,name='mineral_catalog'),
+    path('', views.mineral_catalog_list, name='index'),
+    path('mineral_catalog/',views.mineral_catalog_list,name='mineral_catalog'),
 
 ]
