19e28f6b213df3cad2a3e4553cc7c1e24c35043a
Edima <edimaudo@gmail.com>
2020-06-05 18:16:04 -0400

    Update routes.py

--- fullstack_side_projects/LevelUp/foodworks/app/routes.py
+++ fullstack_side_projects/LevelUp/foodworks/app/routes.py
@@ -1,7 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/add_product')
+def add_product():
+	return render_template("add_product.html")
+
+
+@app.route('/view_product')
+def view_product():
+	return render_template("view_product.html")
+
+@app.route('/edit_product')
+def edit_product():
+	return render_template("edit_product.html")
