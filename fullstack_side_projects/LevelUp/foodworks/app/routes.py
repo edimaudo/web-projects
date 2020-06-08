83e5c68f67e91e263e1f7c3eac14f2653145bfe4
Edima <edimaudo@gmail.com>
2020-06-08 19:56:10 -0400

    Update routes.py

--- fullstack_side_projects/LevelUp/foodworks/app/routes.py
+++ fullstack_side_projects/LevelUp/foodworks/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, flash, redirect, url_for, request
 from app.models import Product
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -9,17 +10,17 @@ def index():
 
 @app.route('/add_product', methods=["GET", "POST"])
 def add_product():
-	if request.method == 'POST':
-		title = request.form['title']
-		price = request.form['price']
-		description = request.form['description']
-		image_url = request.form['image_url']
-		#stock = request.form['stock']
-		my_data = Product(title, price, description, image_url) #add stock later
-		db.session.add(my_data)
-		db.session.commit()
-		flash("Product Added succesfully")
-	return redirect(url_for('index'))
+	# if request.method:
+	# 	title = request.form['title']
+	# 	price = request.form['price']
+	# 	description = request.form['description']
+	# 	image_url = request.form['image_url']
+	# 	#stock = request.form['stock']
+	# 	my_data = Product(title, price, description, image_url) #add stock later
+	# 	db.session.add(my_data)
+	# 	db.session.commit()
+	# 	flash("Product Added succesfully")
+	return redirect(url_for('index'))#redirect(url_for('index'))
 
 
 
