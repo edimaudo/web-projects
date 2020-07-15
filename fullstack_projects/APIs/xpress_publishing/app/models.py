513620cb42702202c6e365cceaa57d6ae6af8a20
Edima <edimaudo@gmail.com>
2020-07-14 21:22:34 -0400

    Update models.py

--- fullstack_projects/APIs/xpress_publishing/app/models.py
+++ fullstack_projects/APIs/xpress_publishing/app/models.py
@@ -3,11 +3,11 @@ from app import db
 
 class Artist(db.model): #update the logic
     __tablename__ = 'artist'
-    id = Column(Integer, primary_key=True)
-    artist_name = Column(String(250), nullable= False)
-    date_of_birth = Column(String(10), nullable=False)
-    biography = Column(String(500), nullable=False)
-    is_currently_employed = Column(Integer, default = 1)
+    id = Column(db.Integer, primary_key=True)
+    artist_name = Column(db.String(250), nullable= False)
+    date_of_birth = Column(db.DateTime, nullable=False)
+    biography = Column(db.String(500), nullable=False)
+    is_currently_employed = Column(db.Integer, default = 1)
     issues = db.relationship('Issue', backref='artist')
 
     def __init__(self, artist_name):
@@ -31,9 +31,9 @@ class Artist(db.model): #update the logic
 
 class Series(db.model): #update the remaining portion
     __tablename__ = 'series'
-    id = Column(Integer, primary_key=True)
-    series_name = Column(String(250), nullable=False)
-    description = Column(String(500), nullable=False)
+    id = Column(db.Integer, primary_key=True)
+    series_name = Column(db.String(250), nullable=False)
+    description = Column(db.String(500), nullable=False)
     issues = db.relationship('Issue', backref='series')
 
     def __init__(self, series_name):
@@ -59,12 +59,12 @@ class Series(db.model): #update the remaining portion
 
 class Issue(db.model): #update remaining part
     __tablename__ = 'issue'
-    id = Column(Integer, primary_key=True)
-    issue_name = Column(String(250), nullable = False)
-    issue_number = Column(Integer, nullable = False)
-    publication_date = Column(String(10), nullable = False)
-    artist_id = Column(Integer, ForeignKey('artist.id'), nullable = False)
-    series_id = Column(Integer, ForeignKey('series.id'), nullable = False)
+    id = Column(db.Integer, primary_key=True)
+    issue_name = Column(db.String(250), nullable = False)
+    issue_number = Column(db.Integer, nullable = False)
+    publication_date = Column(db.DateTime, nullable = False)
+    artist_id = Column(db.Integer, ForeignKey('artist.id'), nullable = False)
+    series_id = Column(db.Integer, ForeignKey('series.id'), nullable = False)
 
     def __init__(self, issue_name):
         """initialize with name."""
