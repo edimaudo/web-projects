3eb03de947080ea126d77ab85cf59fa927ba6bf1
Edima <edimaudo@gmail.com>
2020-11-29 16:33:33 -0500

    Update routes.py
    add contact

--- fullstack_projects/contact_manager/app/routes.py
+++ fullstack_projects/contact_manager/app/routes.py
@@ -5,4 +5,20 @@ from flask import render_template, flash, redirect, url_for
 @app.route('/index')
 def index():
 	all_data = Product.query.all()
-	return render_template('index.html', products = all_data)
+	return render_template('index.html', contacts = all_data)
+
+@app.route('/add_contact/', method=['GET','POST'])
+def add_contact():
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone_number = request.form['phone_number']
+        email = request.form['email']
+        note = request.form['note']
+        my_data = Contact(first_name, last_name, phone_number, email, note)
+        db.session.add(my_data)
+        db.session.commit()
+        flash("Contact Added Sucessfully")
+        return redirect(url_for("index"))
+    return render_template("add_contact.html")
+
