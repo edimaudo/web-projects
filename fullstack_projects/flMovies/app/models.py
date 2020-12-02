d176f210cca67ef86caf6f4cc8e9c4bf9fd7bff9
Edima <edimaudo@gmail.com>
2020-12-01 19:06:51 -0500

    Add files via upload

---
+++ fullstack_projects/flMovies/app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    genre = db.Column(db.String(50))
+    release_date = db.Column(db.DateTime)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Movie {}>'.format(self.title) 
+
+    def __init__(self, tile, genre, release_date, price): 
+    	self.title = title
+    	self.genre = genre
+    	self.release_date = release_date
+    	self.price = price
