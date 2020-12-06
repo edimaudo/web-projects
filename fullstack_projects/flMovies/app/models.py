ce64890e928011fa7a0af6e5ffeb48c1da77b297
Edima <edimaudo@gmail.com>
2020-12-06 15:55:54 -0500

    Update models.py

--- fullstack_projects/flMovies/app/models.py
+++ fullstack_projects/flMovies/app/models.py
@@ -4,13 +4,13 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     genre = db.Column(db.String(50))
-    release_date = db.Column(db.DateTime)
+    release_date = db.Column(db.String(50))
     price = db.Column(db.Float)
 
     def __repr__(self):
         return '<Movie {}>'.format(self.title) 
 
-    def __init__(self, tile, genre, release_date, price): 
+    def __init__(self, title, genre, release_date, price): 
     	self.title = title
     	self.genre = genre
     	self.release_date = release_date
