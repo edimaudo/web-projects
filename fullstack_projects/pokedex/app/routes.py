60bc08d823dfd8b4eb46103be74b30306ea844de
Edima <edimaudo@gmail.com>
2020-12-14 12:38:37 -0500

    Update routes.py

--- fullstack_projects/pokedex/app/routes.py
+++ fullstack_projects/pokedex/app/routes.py
@@ -1,18 +1,17 @@
 from app import app
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, jsonify, make_response
 from app import db
 from app.models import Pokemon
+#from flask_httpauth import HTTPBasicAuth
+
+#error handlers
 
 
-
+#index route
 @app.route('/')
 @app.route('/index')
 def index():
     return "hello world"
-
-
-#index route
-
 #search route
 
 
