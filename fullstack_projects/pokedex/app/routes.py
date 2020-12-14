06c1e0d491278da0cd2ac648b91905e98edbf57c
Edima <edimaudo@gmail.com>
2020-12-14 12:59:14 -0500

    Update routes.py

--- fullstack_projects/pokedex/app/routes.py
+++ fullstack_projects/pokedex/app/routes.py
@@ -4,6 +4,15 @@ from app import db
 from app.models import Pokemon
 #from flask_httpauth import HTTPBasicAuth
 
+def make_pokemon(pokemon):
+    new_pokemon = {}
+    for field in pokemon:
+        if field == 'id':
+            new_pokemon['uri'] = url_for('get_pokemon', pokemon_id=pokemon['id'],_external=True)
+        else:
+            new_pokemon[field] = pokemon[field]
+    return new_pokemon
+
 #error handlers
 @app.errorhandler(400)
 def bad_request(error):
@@ -14,10 +23,18 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}),404)
 
 #index route
-@app.route('/')
-@app.route('/index')
+@app.route('/pokemon/api/v1.0/all_pokemon',methods=['GET'])
 def index():
-    return "hello world"
+    all_data = Pokemon.query.all()
+    return jsonify({'pokemons': [make_pokemon(pokemon) for pokemon in pokemons]})
+
+@app.route('/pokemon/api/v1.0/all_pokemon<int:pokemon_id>', methods=['GET'])
+def get_pokemon(pokemon_id):
+    pokemon = [pokemon for pokemon in pokemons if pokemon['id'] == pokemon_id]
+    if len(pokemon) == 0:
+        abort(404)
+    return jsonify({'pokemon': make_pokemon(pokemon[0])})
+
 #search route
 
 
