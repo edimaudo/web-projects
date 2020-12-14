6e982966f72afe432c8e47bd41ca75e78c7b30c6
Edima <edimaudo@gmail.com>
2020-12-14 12:42:53 -0500

    Update routes.py

--- fullstack_projects/pokedex/app/routes.py
+++ fullstack_projects/pokedex/app/routes.py
@@ -5,7 +5,13 @@ from app.models import Pokemon
 #from flask_httpauth import HTTPBasicAuth
 
 #error handlers
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}),400)
 
+@app.errorhandler(404):
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}),404)
 
 #index route
 @app.route('/')
