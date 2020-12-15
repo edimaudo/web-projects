2db0e10b17a1468be4717774fe7ed8c4e98df96e
Edima <edimaudo@gmail.com>
2020-12-15 12:47:20 -0500

    Update __init__.py

--- fullstack_projects/pokedex/app/__init__.py
+++ fullstack_projects/pokedex/app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 app.config.from_object(Config)
